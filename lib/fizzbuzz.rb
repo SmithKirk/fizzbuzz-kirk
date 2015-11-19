b84d4e3463b191cd20a914b84b637a0f55613464
Kirk Smith <kirks@hotmail.co.uk>
2015-11-19 14:39:33 +0000

    refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,12 @@
 def fizzbuzz number
-  return 'fizzbuzz' if (number % 3 == 0) && (number % 5 == 0)
-  return 'fizz' if number % 3 == 0
-  return 'buzz' if number % 5 == 0
+  return 'fizzbuzz' if is_div_by?(number,3) && is_div_by?(number,5)
+  return 'fizz' if is_div_by?(number,3)
+  return 'buzz' if is_div_by?(number,5)
 
   number
+end
+
+def is_div_by?(num,div)
+  num % div == 0
+
 end
